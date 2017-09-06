92f3d77e2a2984b586c09e4e6350d66802bd9745
Carolina Gonzales <caro.gonz91@gmail.com>
2017-09-05 23:21:22 -0500

    i added crab backgrounds

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use pencil::{Pencil, Request, Response, PencilResult}; //from the crate we want 
 
 use motivations::MOTIVATIONS;
 
-fn handler(_: &mut Request) -> PencilResult { //this function will receive a request. 
+use std::collections::BTreeMap;
+
+pub fn handler(_: &mut Request) -> PencilResult { //this function will receive a request. 
     println! ("Received Request");
    // Ok(Response::from("Hello World!")) //the Ok needs a response based on the Pencil Result code. It needs a response from this string called Hello World! 
    let result=pick_one::pick_one_str(&MOTIVATIONS).to_string();
@@ -14,8 +16,18 @@ fn handler(_: &mut Request) -> PencilResult { //this function will receive a req
 }
 
 fn main() { //writing the server 
-    let mut server = Pencil::new("/web/hello");
-    server.get("/", "hello", handler); //when a client accesses the endpoint "Hello" you have to handle the access with a handler (hello function in line 5) 
-    server.run("127.0.0.1:5000"); //special address that refers to my computer. 127.0.0.1 = 0.0.0.0. = local host. Every address has 65,000 ports. Anything above 1,024 is safe to unrestrictive. 
+    let mut server = Pencil::new("./src");
+     server.enable_static_file_handling(); 
+     server.register_template("motivation.html");
+     server.get("/", "hello", motivation); //when a client accesses the endpoint "Hello" you have to handle the access with a handler (hello function in line 5) 
+     server.run("127.0.0.1:5000"); //special address that refers to my computer. 127.0.0.1 = 0.0.0.0. = local host. Every address has 65,000 ports. Anything above 1,024 is safe to unrestrictive. 
 } 
 
+pub fn motivation(request: &mut Request) -> PencilResult {
+    let mut context = BTreeMap::new();
+    let motivation = pick_one::pick_one_str(&MOTIVATIONS).to_string();
+    let crab = pick_one::pick_one_str(&["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]).to_string();
+    context.insert("motivation".to_string(), motivation);
+    context.insert("image".to_string(), crab);
+    return request.app.render_template("motivation.html", &context);
+}
